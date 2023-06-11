702b7086e0a17d945cd6056ebc24227ab5371161
BeiChienThan <103420854+BeiChienThan@users.noreply.github.com>
2023-06-12 02:30:23 +0700

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1703,6 +1703,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+           void *envp, int *flags); 
+			      
 /*
  * sys_execve() executes a new program.
  */
